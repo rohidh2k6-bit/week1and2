6a5d130f9ca27457676cc6c4470e41cfcc3f16da
duttasubhasis2006-max <duttasubhasis2006@gmail.com>
2026-03-10 09:24:02 +0530

    UC3 DNS Cache with TTL using HashMap

--- App/src/week1and2.java
+++ App/src/week1and2.java
@@ -2,38 +2,57 @@ import java.util.*;
 
 public class week1and2 {
 
-    static HashMap<String, Integer> stock = new HashMap<>();
-    static LinkedList<Integer> waitingList = new LinkedList<>();
+    static class DNSEntry {
+        String ip;
+        long expiryTime;
 
-    public static int checkStock(String productId) {
-        return stock.getOrDefault(productId, 0);
+        DNSEntry(String ip, int ttl) {
+            this.ip = ip;
+            this.expiryTime = System.currentTimeMillis() + (ttl * 1000);
+        }
+
+        boolean isExpired() {
+            return System.currentTimeMillis() > expiryTime;
+        }
     }
 
-    public static String purchaseItem(String productId, int userId) {
+    static HashMap<String, DNSEntry> cache = new HashMap<>();
+    static int hits = 0;
+    static int misses = 0;
 
-        int currentStock = stock.getOrDefault(productId, 0);
+    public static String resolve(String domain) {
 
-        if(currentStock > 0){
-            stock.put(productId, currentStock - 1);
-            return "Success, " + (currentStock - 1) + " units remaining";
+        if (cache.containsKey(domain)) {
+            DNSEntry entry = cache.get(domain);
+
+            if (!entry.isExpired()) {
+                hits++;
+                return "Cache HIT → " + entry.ip;
+            }
         }
-        else{
-            waitingList.add(userId);
-            return "Added to waiting list, position #" + waitingList.size();
-        }
+
+        misses++;
+        String newIP = "172.217.14." + new Random().nextInt(255);
+        cache.put(domain, new DNSEntry(newIP, 5));
+
+        return "Cache MISS → New IP: " + newIP;
+    }
+
+    public static void getStats() {
+        int total = hits + misses;
+        double hitRate = total == 0 ? 0 : (hits * 100.0 / total);
+
+        System.out.println("Hits: " + hits);
+        System.out.println("Misses: " + misses);
+        System.out.println("Hit Rate: " + hitRate + "%");
     }
 
     public static void main(String[] args) {
 
-        stock.put("IPHONE15_256GB", 5);
+        System.out.println(resolve("google.com"));
+        System.out.println(resolve("google.com"));
+        System.out.println(resolve("facebook.com"));
 
-        System.out.println("Stock: " + checkStock("IPHONE15_256GB"));
-
-        System.out.println(purchaseItem("IPHONE15_256GB",12345));
-        System.out.println(purchaseItem("IPHONE15_256GB",67890));
-        System.out.println(purchaseItem("IPHONE15_256GB",11111));
-        System.out.println(purchaseItem("IPHONE15_256GB",22222));
-        System.out.println(purchaseItem("IPHONE15_256GB",33333));
-        System.out.println(purchaseItem("IPHONE15_256GB",44444));
+        getStats();
     }
 }
